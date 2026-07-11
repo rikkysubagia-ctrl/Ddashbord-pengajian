b2d5966a8c2784cc22850f3ba9e378853baf2696
rikkysubagia-ctrl <rikkysubagia@gmail.com>
2026-07-11 23:23:03 +0700

    Refactor script.js by removing styles and adding counter
    Removed CSS styles and added a function to increment a counter.

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-body{
-margin:0;
-font-family:Arial;
-display:flex;
-background:#f2f2f2;
-}
+let quran = 0;
 
-.sidebar{
-width:220px;
-background:#00695c;
-height:100vh;
-padding:20px;
+function tambahData(){
+    quran++;
+    document.getElementById("quran").innerHTML = quran;
 }
-
-.sidebar h2{
-color:white;
-}
-
-.sidebar a{
-display:block;
-color:white;
-text-decoration:none;
-padding:10px;
-margin:5px 0;
-}
-
-.content{
-padding:20px;
-flex:1;
-}
-
-.card{
-background:white;
-padding:20px;
-margin-bottom:15px;
-border-radius:10px;
-box-shadow:0 2px 8px rgba(0,0,0,.2);
-    }
